72bf706d9f1f791ea438c553e68791d91a85c181
David <gelmut@praweb.ru>
2025-02-14 08:18:46 +0500

    Правки.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-/*if (APP_DEBUG) {
-    ini_set('display_errors', 'On');
-} else {
-    ini_set('error_log',  dirname(__DIR__) . '/tmp/errors/error.log');
-}*/
 require_once __DIR__ . '/../config/core.php';
 require_once ROOT . '/vendor/autoload.php';
 
